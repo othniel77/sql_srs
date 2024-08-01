37169ba3ea8fc33124a5d72311938c9992c865f9
root <root@LAPTOP-7EG6JJCD>
2024-08-01 16:40:46 +0200

    chat

--- jojo.py
+++ jojo.py
@@ -8,7 +8,7 @@ tab1, tab2, tab3 = st.tabs(["cat", "dog", "owl"])
 
 with tab1:
     st.header("A cat")
-    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
 
 with tab2:
     st.header("A dog")
