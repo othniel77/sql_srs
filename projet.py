2fc3f9d2fe81c0ba553295e57854e1939fdd1687
root <root@LAPTOP-7EG6JJCD>
2024-08-02 06:39:53 +0200

    rm_duckdb

--- projet.py
+++ projet.py
@@ -7,20 +7,26 @@ st.write("""
 Spaced Repetition System SQL practice
 """)
 
+with st.sidebar:
+    option = st.selectbox(
+        "What would you like to be review?",
+        ("Join","Group By","Windows Functions"),
+        index=None,
+        placeholder="Select a theme...",
+    )
 
-option = st.selectbox(
-    "What would you like to be review?",
-    ("Join","Group By","Windows Functions"),
-    index=None,
-    placeholder="Select a theme...",
-)
+    st.write('You selected:', option)
 
-st.write('You selected:', option)
+data = {"a": [1, 2, 3], "b":[4, 5, 6]}
+lait = pd.DataFrame(data)
+
 tab1, tab2, tab3 = st.tabs(["cat", "dog", "owl"])
 
 with tab1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+    sql_query = st.text_area(label="entrez votre input")
+    result = duckdb.query(sql_query).df()
+    st.write(f"Vous avez entré la query suivante: {sql_query}")
+    st.dataframe(result)
 
 with tab2:
     st.header("A dog")
